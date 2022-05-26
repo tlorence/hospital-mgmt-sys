98cd9c9509be3feb38be93b4c343c984907e0d86
Ashmhmd <82798263+ashmhmd25321@users.noreply.github.com>
2022-05-26 12:10:02 +0530

    finalized

---
+++ Frontend/app/src/Components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Header.css";
+import DriverList from "../../Pages/Ambulance/AmbulanceList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+export default function Header(props) {
+  return (
+    <div className="header">
+      <div className="row">
+        <div className="col">
+          <h5 className="ms-4 mt-1 text-green">{props.topic ?? ""}</h5>
+        </div>
+        <div className="col d-flex justify-content-end">
+          <div className="dropdown me-4">
+            <a
+              href="#"
+              className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+              id="dropdownUser1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://github.com/mdo.png"
+                alt=""
+                width="32"
+                height="32"
+                className="rounded-circle me-2"
+              />
+              <strong>{localStorage.getItem("name").toString}</strong>
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-dark text-small shadow"
+              aria-labelledby="dropdownUser1"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Profile
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
